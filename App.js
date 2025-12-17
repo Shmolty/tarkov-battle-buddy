0c2d0c6a67aeadbdda809981b3033741a1e503e6
Colton <zombiesfreak09@gmail.com>
2025-12-17 16:10:30 -0700

    basic-background-layout

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.rootContainer}>
-      <Text>Hello Tarkov!</Text>
-    </View>
+    <>
+    <StatusBar style="light" />
+      <View style={styles.rootContainer}>
+        <LinearGradient
+          colors={['black', '#06180cff']}
+          style={styles.rootContainer}
+        >
+          <ImageBackground
+            source={require('./assets/images/background.jpg')}
+            resizeMode="cover"
+            style={styles.rootContainer}
+            imageStyle={styles.backgroundImage}
+          >
+            <View style={styles.rootContainer}>
+              
+            </View>
+
+          </ImageBackground>
+        </LinearGradient>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
