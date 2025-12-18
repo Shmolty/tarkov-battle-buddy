31a44ae16fa18b9424f865756c39c9479c09e7e6
Colton <zombiesfreak09@gmail.com>
2025-12-17 19:29:17 -0700

    Create starting layout create the look and feel of the app i.e. title, content, nav layout create HomeScreen and set to a flatlist with placeholder content for testing return default assets for spash screen to avoid errors

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 // REACT NATIVE AND EXPO IMPORTS
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, View, ImageBackground, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
 
 // CUSTOM COMPONENT IMPORTS
 import Title from './components/Title';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
@@ -23,11 +24,15 @@ export default function App() {
             style={styles.rootContainer}
             imageStyle={styles.backgroundImage}
           >
-            
+
             <View style={styles.contentContainer}>
 
               <Title />
+
+              <HomeScreen />
               
+              <Text style={styles.navText}>At the bottom of every screen will be a navigation pane</Text>
+            
             </View>
 
           </ImageBackground>
@@ -45,7 +50,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  screen: {
+    flex: 1,
+  },
   backgroundImage: {
     opacity: 0.15,
   },
+
+  // temp
+    navText: {
+        justifyContent: 'flex-end',
+        marginBottom: 65,
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+    },
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,44 @@
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+// filler content for prototyping. will be replaced
+const content = [
+    'Placeholder content goes here. I am just creating an array of strings as an example',
+    'Here is another sentence that is better off unread. It is a waste of time to sit here and read through this stuff.',
+    'Wait are you seriously still reading this? I thought i was clear, this is just filler content.',
+    'Well if you are going to keep reading I might as well fill you in on my biggest secret. You will have to scroll through some stuff to find it though.',
+    'Blah blah blee blee blah. -Aristotle',
+    'You know the secret is in here somewhere but I cannot just put it in the bottom and make it easy for you.',
+    'Have you noticed I avoid using \' in this text? It\'s really not hard to do but I am lazy and thus will not be doing it much',
+    'The big secret is I have no idea what I am doing',
+    'There\'s something special about writing a bunch of nonsense just to fill a screen. Kind of like a personal diary I guess.',
+    'I am really running out of things to say so I think I will just stop here for now. Just enough content to scroll through',
+];
+
+function HomeScreen () {
+    return (
+        <View style={styles.rootScreen}>
+                <FlatList
+                    data={content}
+                    keyExtractor={(item) => item.index}
+                    renderItem={(itemData) => <View key={itemData.index} style={styles.listItem}><Text style={styles.contentText}>{itemData.item}</Text></View>}
+                    alwaysBounceVertical={false}
+                />
+        </View>
+    )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    rootScreen: {
+        flex: 1,
+        padding: 15,
+    },
+    listItem: {
+        padding: 15,
+    },
+    contentText: {
+        color: 'white',
+        fontSize: 24,
+    },
+});
