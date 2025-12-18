3a88b9b047b8e4734cc58b1956d7d1f08d093d6c
Colton <zombiesfreak09@gmail.com>
2025-12-18 00:03:27 -0700

    add screen switching functionality

--- App.js
+++ App.js
@@ -1,24 +1,56 @@
 // REACT NATIVE AND EXPO IMPORTS
-import { StyleSheet, View, ImageBackground, Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, ImageBackground } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
 
 // CUSTOM COMPONENT IMPORTS
 import Title from './components/Title';
 import HomeScreen from './screens/HomeScreen';
-import NavBar from './components/NavBar';
+import NavBar from './components/nav/NavBar';
 
 export default function App() {
+  const [title, setTitle] = useState('TARKOV BATTLE BUDDY');
+  const [screen, setScreen] = useState(<HomeScreen />);
+
+  function changeScreen (newScreen) {
+    switch (newScreen) {
+      // the case statements are directly tied to icon names passed from NavBar
+      case 'home':
+        console.log('screen changed to home');
+        setTitle('TARKOV BATTLE BUDDY');
+        setScreen(<HomeScreen />);
+        break;
+
+      case 'map-marked-alt':
+        console.log('screen changed to map');
+        setTitle ('MAP SCREEN');
+        setScreen(<View><Title>Map stuff go here</Title></View>);
+        break;
+
+      case 'database':
+        console.log('screen changed to item data');
+        setTitle('Item Lookup');
+        setScreen(<View><Title>Item lookup stuff go here</Title></View>);
+        break;
+
+      case 'account':
+        console.log('screen changed to account');
+        setTitle('Account');
+        setScreen(<View><Title>Account info here</Title></View>);
+        break;
+    }
+  }
+
+
   return (
     <>
     <StatusBar style="light" />
       <View style={styles.rootContainer}>
-
         <LinearGradient
           colors={['black', '#06180cff']}
           style={styles.rootContainer}
         >
-
           <ImageBackground
             source={require('./assets/images/background.jpg')}
             resizeMode="cover"
@@ -27,17 +59,14 @@ export default function App() {
           >
 
             <View style={styles.contentContainer}>
-              
+
               <View style={styles.title}>
-                <Title />
+                <Title>{title}</Title>
               </View>
               
+              {screen}
 
-              <HomeScreen />
-
-              <NavBar />
-
-              {/* <Text style={styles.navText}>At the bottom of every screen will be a navigation pane</Text> */}
+              <NavBar onNavButtonPress={changeScreen} />
 
             </View>
 
