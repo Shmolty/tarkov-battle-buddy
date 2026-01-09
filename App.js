5c0b4fc20fbd6c2851bfacef2895f5c86c332290
Colton <zombiesfreak09@gmail.com>
2026-01-09 12:03:33 -0700

    remove custom nav from project delete custom nav files and imports

--- App.js
+++ App.js
@@ -7,41 +7,8 @@ import { LinearGradient } from 'expo-linear-gradient';
 // CUSTOM COMPONENT IMPORTS
 import Title from './components/Title';
 import HomeScreen from './screens/HomeScreen';
-import NavBar from './components/nav/NavBar';
 
 export default function App() {
-  const [title, setTitle] = useState('TARKOV BATTLE BUDDY');
-  const [screen, setScreen] = useState(<HomeScreen />);
-
-  function changeScreen (newScreen) {
-    switch (newScreen) {
-      // the case statements are directly tied to icon names passed from NavBar
-      case 'home':
-        console.log('screen changed to home');
-        setTitle('TARKOV BATTLE BUDDY');
-        setScreen(<HomeScreen />);
-        break;
-
-      case 'map-marked-alt':
-        console.log('screen changed to map');
-        setTitle ('MAP SCREEN');
-        setScreen(<View><Title>Map stuff go here</Title></View>);
-        break;
-
-      case 'database':
-        console.log('screen changed to item data');
-        setTitle('Item Lookup');
-        setScreen(<View><Title>Item lookup stuff go here</Title></View>);
-        break;
-
-      case 'account':
-        console.log('screen changed to account');
-        setTitle('Account');
-        setScreen(<View><Title>Account info here</Title></View>);
-        break;
-    }
-  }
-
 
   return (
     <>
@@ -61,12 +28,8 @@ export default function App() {
             <View style={styles.contentContainer}>
 
               <View style={styles.title}>
-                <Title>{title}</Title>
+                <Title>Title Here</Title>
               </View>
-              
-              {screen}
-
-              <NavBar onNavButtonPress={changeScreen} />
 
             </View>
 
